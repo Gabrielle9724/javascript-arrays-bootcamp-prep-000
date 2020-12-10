2eab1fa849814cb3de91b5be410f3d427a8eff18
Gabrielle Piacentile <gabriellepiacentile98@gmail.com>
2020-12-10 16:25:34 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,5 +1,20 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 var candyString = "twix"
+
 function addElementToBeginningOfArray (chocolateBars, candyString){
   return [candyString, ...chocolateBars];
+}
+
+function destructivelyAddElementToBeginningOfArray (chocolateBars, candyString){
+  chocolateBars.unshift (candyString);
+  return chocolateBars
+}
+
+function addElementToEndOfArray (chocolateBars, candyString){
+  return [chocolateBars, ...candyString];
+}
+
+function destructivelyAddElementToEndOfArray (chocolateBars, candyString){
+  chocolateBars.push (candyString);
+  return chocolateBars
 }
